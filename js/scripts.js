1709d5c8ea0e7f69f8acae8d880d26b8fe523447
peninah-njeri <peninahgathuru@gmail.com>
2019-08-31 14:52:54 +0300

    modify js file

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// // Business Logic //
+
+// // naming function to call upon later //
+// var Pizza= function(size, topping){
+//     this.size = size;
+//     this.toppings = toppings;
+//   }
+  
+//     Pizza.prototype.addPepperoni = function(){
+//       return this.peperoni = true;
+//       console.log(pepperoni);
+//     }
+//     Pizza.prototype.addPineapple = function(){
+//     return this.pineapple = true;
+//     }
+  
+  
+//   // User Interface Logic //
+//   $(document).ready(function() {
+//     $("#formOne").submit(function(event) {
+//       event.preventDefault();
+  
+//       var newPizza = new Pizza(size, toppings);
+  
+//       var size = $("#sizeSelect option:selected").text();
+//       var toppings = $("#toppingsSelect option:selected").text();
+//       var sizeValue = parseInt($("#sizeSelect option:selected").val();
+//       var toppingsValue = parseInt($("#toppingsSelect option:selected").val();
+  
+//       var totalPrice = sizeValue + toppingsValue;
+  
+//     if(newPizza.pepperoni.
+  
+//   )};
