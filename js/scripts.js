661e0ff1e5b62214642599f20e328a8ee8de5528
peninah-njeri <peninahgathuru@gmail.com>
2019-09-01 16:29:41 +0300

    add user interface logic of address details

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,98 @@
-// // Business Logic //
-
-// // naming function to call upon later //
-// var Pizza= function(size, topping){
+// user interface
+function peshy(){
+    var email = document.getElementById("email").value;
+    var name = document.getElementById("name").value;
+    var location = document.getElementById("location").value;
+  
+    // business logic
+//     if (email.length < 10) {
+//       alert("email invalid!");
+//     }
+//     if (name.length < 1 || name.length == 0) {
+//       alert("please enter full name!");
+//     }
+//     if (location.length < 1) {
+//       alert("Please enter a valid location");
+//     }
+//     if (email.length > 10  && name.length > 1 && location.length > 1) {
+//       alert("Dear" + name + " of email: " + email + " from " + location + " we have received your message!")
+//     }
+//   };
+  
+//   // User interface
+//   function Charge(size, crust, topping,delivery) {
 //     this.size = size;
-//     this.toppings = toppings;
+//     this.crust = crust;
+//     this.topping = topping;
+//     this.delivery = delivery
+//     this.price = 0;
+//    }
+//    var totalPrice = [];
+  
+//    var pizzaSizes = ["small", "medium", "large"];
+//    var crust = ["Crispy", "Stuffed", "Gluten-free"];
+//    var topping = ["Pepperoni", "Chicken", "Bacon", "Sausage", "Mushrooms"];
+//    var delivery = ["deliver", "dont-deliver"];
+  
+//   //  Business logic
+//    Charge.prototype.totalPrice = function () {
+//     if (this.size === pizzaSizes["small"]) {
+//       this.price += 750;
+//     } if (this.size === pizzaSizes["medium"]) {
+//       this.price += 1000;
+//     } else if (this.size === pizzaSizes["large"]) {
+//       this.price += 1500;
+//     }
+//     if (this.crust === crust["Cripsy"]) {
+//       this.price += 100;
+//     } else if (this.crust === crust["Stuffed"]) {
+//       this.price += 150;
+//     } else if (this.crust === crust["Gluten-free"]) {
+//       this.price += 200;
+//     }
+//     if (this.topping === topping["Pepperoni"]) {
+//       this.price += 100;
+//     } else if (this.topping === topping["Chicken"]) {
+//       this.price += 200;
+//     } else if (this.topping === topping["Bacon"]) {
+//       this.price += 300;
+//     } else if (this.topping === topping["Sausage"]) {
+//       this.price += 50;
+//     }else if (this.topping === topping["Mushrooms"]) {
+//       this.price += 70;
+//     }
+//     if (this.delivery === delivery["dont-deliver"]) {
+//       this.price += 0;
+//     }else if (this.deliver === delivery["deliver"]) {
+//       this.price += 200;
+//     }
+//     return this.price;
 //   }
   
-//     Pizza.prototype.addPepperoni = function(){
-//       return this.peperoni = true;
-//       console.log(pepperoni);
-//     }
-//     Pizza.prototype.addPineapple = function(){
-//     return this.pineapple = true;
-//     }
   
+//   Charge.prototype.totalCost = function () {
+//     var cartTotal = 0;
+//     for (var shopping = 0; shopping <totalPrice.length; shopping ++) {
+//       cartTotal +=totalPrice[shopping];
+//     }
+//     return cartTotal;
+//    }
   
-//   // User Interface Logic //
-//   $(document).ready(function() {
-//     $("#formOne").submit(function(event) {
+//    $(document).ready(function () {
+//     $("input#cost").click(function (event) {
 //       event.preventDefault();
+//       var sizes = $("select#PizzaSizes").val();
+//       var crusts = $("select#crust").val();
+//       var toppings = $("select#toppings").val();
+//       var deliver = $("select#deliver").val();
   
-//       var newPizza = new Pizza(size, toppings);
-  
-//       var size = $("#sizeSelect option:selected").text();
-//       var toppings = $("#toppingsSelect option:selected").text();
-//       var sizeValue = parseInt($("#sizeSelect option:selected").val();
-//       var toppingsValue = parseInt($("#toppingsSelect option:selected").val();
-  
-//       var totalPrice = sizeValue + toppingsValue;
-  
-//     if(newPizza.pepperoni.
-  
-//   )};
+//       var newPrice = new Charge(sizes, crusts, toppings);
+//       newPrice.totalPrice();
+//       totalPrice.push(newPrice.price);
+//       $("#a").text("pizza :" + sizes);
+//       $("#b").text("crusts  :" + crusts);
+//       $("#typ").text("toppings  :" + toppings);
+//       $("#ty").text("deliver  :" + deliver);
+//       $("#t").text("total cost  :" + newPrice.totalCost());
+//     });
+//    });
