d666b1ee2d7ffe4de61b11074b1da2f0d8b3d70b
peninah-njeri <peninahgathuru@gmail.com>
2019-09-02 09:53:49 +0300

    add space to the alert message

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ function peshy() {
         alert("Please enter a valid location");
     }
     if (email.length > 10 && name.length > 1 && location.length > 1) {
-        alert("Dear" + name + "of email: " + email + " from " + location + " we have received your order and its being worked on!")
+        alert("Dear "+ ", " + name + " of email: " + email + " from  " + location + " we have received your order and its being worked on!")
     }
 };
 
